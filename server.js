5a1f7e7d92fdb1fe84fb95f4c7e002d921137e37
honarab24 <iptvplayer024@gmail.com>
2025-08-21 19:47:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,13 +28,7 @@ app.use(rateLimit({
 
 // ✅ IPTV Streams
 const streams = {
-  gma7: "http://143.44.136.110:6610/001/2/ch00000090990000001093/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2388020250818020007000282&m3u8_level=2&ztecid=ch00000090990000001093&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  cinemoph: "http://143.44.136.110:6610/001/2/ch00000090990000001254/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2339620250818035221000307&m3u8_level=2&ztecid=ch00000090990000001254&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  kapamilyachannelHD: "http://143.44.136.110:6610/001/2/ch00000090990000001286/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2442320250818031101000309&m3u8_level=2&ztecid=ch00000090990000001286&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  gtv: "http://143.44.136.110:6610/001/2/ch00000090990000001143/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2330520250818033205000291&m3u8_level=2&ztecid=ch00000090990000001143&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  cinemaoneph: "http://143.44.136.110:6610/001/2/ch00000090990000001283/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2331920250818234551000900&m3u8_level=2&ztecid=ch00000090990000001283&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  alltv2: "http://143.44.136.110:6610/001/2/ch00000090990000001179/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2342420250819074524001150&m3u8_level=2&ztecid=ch00000090990000001179&virtualDomain=001.live_hls.zte.com&ispcode=55",
-  net25: "http://143.44.136.112:6610/001/2/ch00000090990000001090/sec-f1-v1-a1.m3u8?usersessionid=&IASHttpSessionId=OTT2477320250819074041000881&m3u8_level=2&ztecid=ch00000090990000001090&virtualDomain=001.live_hls.zte.com&ispcode=55",
+
 };
 
 
